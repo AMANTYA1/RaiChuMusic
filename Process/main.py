29d8da9a71a3c4e69414808cf1d4d87ba54bc8cc
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-05-08 19:21:59 +0530

    Update main.py

--- Process/main.py
+++ Process/main.py
@@ -1,3 +1,9 @@
+import os
+import sys
+import random
+import asyncio
+import telethon.utils
+from telethon import TelegramClient, events
 from RaiChu.config import API_HASH, API_ID, BOT_TOKEN, SESSION_NAME
 from pyrogram import Client
 from pytgcalls import PyTgCalls
@@ -10,10 +16,13 @@ bot = Client(
     plugins={"root": "RaiChu.Player"},
 )
 
+BOT = TelegramClient('BOT', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+
 user = Client(
     SESSION_NAME,
     api_id=API_ID,
     api_hash=API_HASH,
 )
 
-call_py = PyTgCalls(user, overload_quiet_mode=True)
+Test = Client(SESSION_NAME, api_id=API_ID, api_hash=API_HASH, plugins={'root': 'RaiChu.Player'})
+call_py = PyTgCalls(Test)
