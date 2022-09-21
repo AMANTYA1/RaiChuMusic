83079931e599fe067603bc4322c533bee3e1bf11
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-09-21 21:03:23 +0530

    Update Buger.py

--- RaiChu/Player/Buger.py
+++ RaiChu/Player/Buger.py
@@ -3,10 +3,3 @@
 from os import remove
 from random import choice
 from carbon.events import register
-
-@register(pattern="^/q ?(.*)")
-
-message = 'Crow Heroku no ban Deploy Successful'
-
-
-print(message)
