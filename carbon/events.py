cb0960804de4690bfb9574864b7ab6139e6a02ea
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-09-21 20:36:35 +0530

    Create events.py

---
+++ carbon/events.py
@@ -0,0 +1,18 @@
+import inspect
+import logging
+import sys
+import re
+
+from pathlib import Path
+from telethon import events
+
+def register(**args):
+    """ Registers a new message. """
+    pattern = args.get("pattern", None)
+
+    r_pattern = r"^[/!.]"
+
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+
+    args["pattern"] = pattern.replace("^/", r_pattern, 1)
