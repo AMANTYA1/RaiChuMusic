9b6b99405c278e7fcbe63d175d56d0d70542fe77
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-09-21 20:57:48 +0530

    Update Buger.py

--- RaiChu/Player/Buger.py
+++ RaiChu/Player/Buger.py
@@ -2,18 +2,11 @@
 
 from os import remove
 from random import choice
-
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
-from telegram.ext import (
-    CallbackContext,
-    CallbackQueryHandler,
-    CommandHandler,
-    Filters,
-    MessageHandler,
-)
-from telethon.tl.functions.users import GetFullUserRequest
 from carbon.events import register
 
 @register(pattern="^/q ?(.*)")
 
-Print ("Crow Heroku no ban Deploy Successful")
+message = 'Crow Heroku no ban Deploy Successful'
+
+
+print(message)
